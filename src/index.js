659aaf25f2435cfa4bf54f26871c09c5fde88241
allison_johnson <allison.cuttler@gmail.com>
2020-03-28 15:18:25 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -9,6 +9,15 @@ function fetchImages() {
   })
 } //function fetchImages
 
+function fetchBreeds() {
+  const breedURL = "https://dog.ceo/api/breeds/list/all"
+  fetch(breedURL).then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    addBreeds(json);
+  })
+} //function fetchImages
+
 function addImages(json) {
   //debugger 
   const dogImages = document.getElementById("dog-image-container");
@@ -17,21 +26,65 @@ function addImages(json) {
       dogImg.src = url;
       dogImages.appendChild(dogImg);
   })
-} //function addImages
+}// function addImages
+
+function addBreeds(json) {
+  const breedList = document.getElementById("dog-breeds");
+  const allBreeds = json["message"];
+
+  for (const key in allBreeds) {
+    if (allBreeds[key].length == 0) {
+      li = document.createElement('li');
+      li.innerHTML = key;
+      breedList.appendChild(li);
+      li.classList.add("breed");
+    }// no sub-breeds
+
+    else {
+      for (let i = 0; i < allBreeds[key].length; i++) {
+        li = document.createElement('li');
+        li.innerHTML = `${key} (${allBreeds[key][i]})`;
+        breedList.appendChild(li);
+        li.classList.add("breed");
+      }// for
+    }// sub-breeds
+
+    breedList.addEventListener("click", function(e) {
+      changeColor(e.target, "teal");
+      //e.target.style.color = "teal";
+    })//eventListener click on list item
+  }// for 
+
+}//function addBreeds 
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchImages();
-})
+  fetchBreeds();
+  const menu = document.getElementById("breed-dropdown");
+  menu.addEventListener('change', function(event) {
+    let allItems = document.getElementsByClassName("breed")
+    for (let i = 0; i < allItems.length; i++) {
+      allItems[i].hidden = false;
+    }//for
 
-/* json =>
-{
-    "message": [
-      "https://images.dog.ceo/breeds/terrier-russell/little1.jpg",
-      "https://images.dog.ceo/breeds/terrier-border/n02093754_373.jpg",
-      "https://images.dog.ceo/breeds/bulldog-english/bunz.jpg",
-      "https://images.dog.ceo/breeds/husky/n02110185_14560.jpg"
-    ],
-    "status": "success"
-  }
-  */
- 
+    const firstLetter = event.target.value;
+    filterBreeds(firstLetter);
+  })//eventListener for dropdown menu
+})//eventListener for DOMContentLoaded
+
+function changeColor(elt, newColor){
+  elt.style.color = newColor;
+}//function changeColor
+
+function filterBreeds(letter) {
+  let allItems = document.getElementsByClassName("breed")
+  for (let i = 0; i < allItems.length; i++) {
+    if (allItems[i].innerText[0] != letter) {
+      allItems[i].hidden = true;
+    }//if
+  }//for
+}//function filterBreeds
+
+
+
+
